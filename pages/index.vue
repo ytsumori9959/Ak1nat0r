5fafd62a44fbe59b4c634a07222976f269953148
Yuta Tsumori <yutatsumori@gmail.com>
2020-09-12 17:35:00 +0900

    Implement creating user document

--- pages/index.vue
+++ pages/index.vue
@@ -2,17 +2,29 @@
   <v-container fluid fill-height>
     <v-layout column justify-center align-center>
       <h1 class="display-4 text-center">Ak1nat0r</h1>
-      <v-btn @click="login">PLAY</v-btn>
+      <v-col cols="12" sm="6" md="3">
+        <v-text-field
+          label="Player Name"
+          placeholder="Akinator"
+          outlined
+          v-model="playerName"
+        ></v-text-field>
+      </v-col>
+      <v-btn @click="signIn">PLAY</v-btn>
     </v-layout>
   </v-container>
 </template>
 
 <script>
 import firebase from '~/plugins/firebase'
+const firestore = firebase.firestore()
 
 export default {
+  data: () => ({
+    playerName: '',
+  }),
   methods: {
-    login() {
+    signIn() {
       firebase
         .auth()
         .signInAnonymously()
@@ -23,7 +35,15 @@ export default {
 
           console.error(error)
         })
-      console.log('You just logged in')
+        .then((userCredential) => this.savePlayerName(userCredential))
+    },
+    savePlayerName(userCredential) {
+      if (this.playerName == '') this.playerName = 'Akinator'
+      firestore
+        .collection('users')
+        .doc(userCredential.user.uid)
+        .set({ playerName: this.playerName })
+        .then(() => console.log('You just logged in'))
     },
   },
 }
